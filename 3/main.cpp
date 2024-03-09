35ce215c277ad590e5e35453ca30a83dccf5fc32
Serafim Sukharev <simasuh@gmail.com>
2024-03-09 22:16:43 +0300

    QStyle

---
+++ 3/main.cpp
@@ -0,0 +1,17 @@
+#include "widget.h"
+
+#include <QApplication>
+
+int main(int argc, char *argv[])
+{
+    // Атрибут нужен для корректного отображения размеров элементов на мониторах с большим разрешением
+    QApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+    // Приложение
+    QApplication a(argc, argv);
+    // Главное окно
+    Widget w;
+    // Показываем главное окно
+    w.show();
+    // Запускаем приложение
+    return a.exec();
+}
